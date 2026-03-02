a3273ffe48bac36a21d683716e0bb3c60b904d76
Sidesh Saini <sideshsaini5678@gmail.com>
2026-03-02 16:42:39 +0530

    update in rghs

--- somacyweb/src/App.js
+++ somacyweb/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import LabReports from "./pages/LabReports";
 import Footer from "./Component/Footer";
 import Header from "./Component/Header";
+import RghsInfo from "./pages/RghsInfo";
 
 function App() {
   return (
     <div className="App">
       <Header />
-    <LabReports />
+      <RghsInfo/>
+    {/* <LabReports /> */}
     <Footer />
     </div>
   );

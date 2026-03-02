4ff6dc5613ca4e2ffbdda868b4e899d87ac459d1
apple <apple@apples-MacBook-Pro.local>
2026-03-02 17:31:47 +0530

    update in cart,my order

--- somacyweb/src/App.js
+++ somacyweb/src/App.js
@@ -4,14 +4,23 @@ import Footer from "./Component/Footer";
 import Header from "./Component/Header";
 import RghsInfo from "./pages/RghsInfo";
 import Homepage from "./pages/homepage";
+import CartPage from "./pages/Cart";
+import { Route, Routes } from "react-router-dom";
+import MyOrder from "./pages/MyOrder";
 
 function App() {
   return (
-    <div className="App">
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <RghsInfo />
-      {/* <LabReports /> */}
-      <Homepage />
+
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/myorder" element={<MyOrder />} />
+          <Route path="/lab-test" element={<RghsInfo />} />
+        </Routes>
+      </main>
       <Footer />
     </div>
   );

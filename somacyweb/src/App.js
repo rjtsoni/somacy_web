fbeaf9e733558704e36ee97b09095db1f2105f85
apple <apple@apples-MacBook-Pro.local>
2026-03-05 10:38:37 +0530

    update in not found page and app

--- somacyweb/src/App.js
+++ somacyweb/src/App.js
@@ -8,20 +8,24 @@ import CartPage from "./pages/Cart";
 import { Route, Routes } from "react-router-dom";
 import MyOrder from "./pages/MyOrder";
 import MedicineComparison from "./pages/MedicineComaprison";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
+    // <div className="min-h-screen flex flex-col">
+    <div className="h-full flex flex-col items-center justify-center bg-slate-50 relative overflow-hidden">
       <Header />
 
       <main className="flex-grow">
+        {/* <main className="flex-grow flex"> */}
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/myorder" element={<MyOrder />} />
           <Route path="/lab-test" element={<RghsInfo />} />
-                    <Route path="/medicine-search" element={<MedicineComparison />} />
+          <Route path="/medicine-search" element={<MedicineComparison />} />
 
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
       <Footer />
